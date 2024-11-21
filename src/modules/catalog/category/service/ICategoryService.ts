fd8881c580cdc0d4371794669fb4b617294069fd
hoangcao <22520457@gm.uit.edu.vn>
2024-11-21 23:54:42 +0700

    docker file

--- src/modules/catalog/category/service/ICategoryService.ts
+++ src/modules/catalog/category/service/ICategoryService.ts
@@ -6,12 +6,11 @@ import {ICondition} from "../../../../libs/condition";
 import {Paging} from "../../../../libs/paging";
 import {IRequester} from "../../../../libs/IRequester";
 import {Category} from "../entity/category";
-import {CategoryFilter} from "../entity/CategoryFilterSchema";
 
 export interface ICategoryService  {
     create(requester : IRequester,c : CategoryCreate): ResultAsync<void, Err>
     update(requester : IRequester,id : number,c : CategoryUpdate): ResultAsync<void, Err>
     delete(requester : IRequester,id : number): ResultAsync<void, Err>
-    list(cond : CategoryFilter, paging : Paging): ResultAsync<Category[] | null , Err>
+    list(cond : ICondition, paging : Paging): ResultAsync<Category[] | null , Err>
     findById(id : number): ResultAsync<Category | null, Err>
 }
