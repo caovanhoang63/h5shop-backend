a15b4a3d85fefc79dba41b4cb30964d90aec19cd
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2024-12-26 15:11:09 +0700

    update filterschema

--- src/modules/inventory/entity/inventoryReportFilterSchema.ts
+++ src/modules/inventory/entity/inventoryReportFilterSchema.ts
@@ -3,7 +3,8 @@ import Joi from "joi";
 
 export const inventoryReportFilterSchema = baseFilterSchema.keys({
     warehouseMan1: Joi.number(),
-    lk_fromDate: Joi.date(),
+    fromDate: Joi.date(),
     toDate: Joi.date(),
+    lk_Id: Joi.number(),
 });
 
