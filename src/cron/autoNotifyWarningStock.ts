64356da2c0f283f3f8966bcb3d4d056af7bcd0a3
Bui Thai Hoang <buithaihoang04gl@gmail.com>
2025-01-03 14:08:35 +0700

    SES client, mail service

--- src/cron/autoNotifyWarningStock.ts
+++ src/cron/autoNotifyWarningStock.ts
@@ -2,14 +2,54 @@ import {CronJob} from "cron";
 import {container} from "../container";
 import {ISkuService} from "../modules/catalog/sku/service/ISkuService";
 import {TYPES} from "../types";
+import {generateEmailTemplate, SesClient} from "../sesClient";
+import {SendEmailCommand} from "@aws-sdk/client-ses";
 
 export const cronAutoNotifyWarningStock = CronJob.from({
-    cronTime: "0 22 * * *",
+    cronTime: "*/30 * * * * *",
     onTick: async () => {
         const skuService = container.get<ISkuService>(TYPES.ISkuService);
         const result = await skuService.findWarningStock();
-        if(result.isOk()){
-            console.log("Found sku warning stock",result.value)
+        if(result.isErr())
+            return;
+        if(!result.value)
+            return;
+
+        console.log("auto mail warning stock");
+
+        if(result.value.length > 0){
+            const sesClient = SesClient
+
+            const emailTemplate = generateEmailTemplate(result.value);
+
+            const inputEmailWaningStock = {
+                Source: "h5inventory@h5shop.shop",
+                Destination: {
+                    ToAddresses: ["buithaihoang04gl@gmail.com"],
+                    CcAddresses: [],
+                    BccAddresses: [],
+                },
+                Message: {
+                    Subject: {
+                        Data: "Cảnh báo mức tồn kho!",
+                        Charset: "UTF-8",
+                    },
+                    Body: {
+                        Html: {
+                            Data: emailTemplate,
+                            Charset: "UTF-8",
+                        },
+                    },
+                },
+            };
+
+            const sendEmailCommand = new SendEmailCommand(inputEmailWaningStock)
+            try {
+                const data = await sesClient.send(sendEmailCommand);
+                console.log("Email sent successfully:", data);
+            } catch (error) {
+                console.error("Error sending email:", error);
+            }
         }
     },
     start: false,
