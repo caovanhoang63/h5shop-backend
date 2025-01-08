7777650e4ced93fc792d82aac01cecd877f27c76
hoangcao <22520457@gm.uit.edu.vn>
2025-01-08 11:42:42 +0700

    fix something

--- src/modules/customer/entity/customerUpdate.ts
+++ src/modules/customer/entity/customerUpdate.ts
@@ -4,10 +4,11 @@ import Joi from "joi";
 
 export interface CustomerUpdate extends BaseModel {
     address?: string;
-    firstName: string;
-    lastName: string;
+    firstName?: string;
+    lastName?: string;
     dateOfBirth? : Date;
     gender: CustomerGender;
+    discountPoint?: number;
 }
 
 export const customerUpdateSchema = Joi.object({
@@ -16,4 +17,5 @@ export const customerUpdateSchema = Joi.object({
     lastName: Joi.string(),
     dateOfBirth: Joi.date().valid(...Object.values(Date)),
     gender: Joi.string().valid(...Object.values(CustomerGender)),
+    discountPoint: Joi.number().optional(),
 })
